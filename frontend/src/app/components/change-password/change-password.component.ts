64124c3d24402da2ac5f4e8da6ee462fe5827f1e
ruthwikreddy07 <179065000+ruthwikreddy07@users.noreply.github.com>
2026-02-12 21:45:54 +0530

    started #429

--- frontend/src/app/components/change-password/change-password.component.ts
+++ frontend/src/app/components/change-password/change-password.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { UserService } from '../../services/user.service';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-change-password',
